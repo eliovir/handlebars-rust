b776f2d7227903510fcc965c7295b90363288f5d
Ning Sun <sunng@about.me>
2016-10-29 12:24:38 +0800

    (cargo-release) generate docs

--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["handlebars"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='handlebars/struct.Template.html' title='handlebars::Template'>Template</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='handlebars/enum.TemplateError.html' title='handlebars::TemplateError'>TemplateError</a>",];
+implementors["handlebars"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='handlebars/struct.Template.html' title='handlebars::Template'>Template</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='handlebars/struct.TemplateError.html' title='handlebars::TemplateError'>TemplateError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
